1db5e0469ddde914ace4a5170212fbe19ab6db44
Naveen Kumar <naveen7020@gmail.com>
2019-11-15 20:39:56 +0530

    Created new ppaptemplatecreatepage and ppaptemplatecreatepagetest files

--- QLMTest/src/main/java/com/qlm/qa/pages/PPAPWorkflowTemplatePage.java
+++ QLMTest/src/main/java/com/qlm/qa/pages/PPAPWorkflowTemplatePage.java
@@ -2,18 +2,65 @@ package com.qlm.qa.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
-public class PPAPWorkflowTemplatePage
+import com.qlm.qa.base.TestBase;
+
+public class PPAPWorkflowTemplatePage extends TestBase
 {
 	
 	//Page Factory
 	
-	@FindBy(xpath="//a[contains(text(),'Norman Hemmings(admin)')]")
-	WebElement usernamelabel;
+   @FindBy(xpath="//button[@id='btn-new-template']")
+   WebElement createNewTemplateButton;
+   
+   @FindBy(xpath="//input[@class='form-control text-box single-line']")
+   WebElement templateName;
 	
+   @FindBy(xpath="//select[@id='form-field-submission-level']")
+   WebElement submissionLevel;
+   
+   @FindBy(id="//button[@id='modal-confirmation-ok']")
+   WebElement templateCreateButton;
 	
+   @FindBy(id="modal-confirmation-cancel")
+   WebElement templateCreatecancelButton;
+   
+   
+   
+   
+   
+   
+   //Actions
 	
-	
-	
+   public PPAPWorkflowTemplatePage()
+   {
+	   PageFactory.initElements(driver, this);
+   }
+   
+   public String validatePPAPWorkflowTemplatePageTitle()
+   {
+	  return driver.getTitle();
+   }
+      
+   public void clickOnCreateNewTemplateButton()
+   {
+	   createNewTemplateButton.click();
+   }
 
+   public void createNewTemplate(String tempName, String subLevel)
+   {
+	   //Thread.sleep(1500);
+	   templateName.sendKeys(tempName);
+	   Select sel = new Select(submissionLevel);
+	   sel.selectByVisibleText(subLevel);
+	   //templateCreateButton.click();
+	   
+   }
+   
+   
+   
+   
+   
 }
---
+++ QLMTest/src/test/java/com/qlm/qa/testcases/PPAPWorkflowTemplatePageTest.java
@@ -0,0 +1,70 @@
+package com.qlm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.qlm.qa.base.TestBase;
+import com.qlm.qa.pages.HomePage;
+import com.qlm.qa.pages.LoginPage;
+import com.qlm.qa.pages.PPAPWorkflowTemplatePage;
+import com.qlm.qa.util.TestUtil;
+
+public class PPAPWorkflowTemplatePageTest extends TestBase
+{
+	LoginPage loginpage;
+	HomePage homePage;
+	PPAPWorkflowTemplatePage ppapworkflowtemplatepage;
+	
+	String sheetName="PPAPTemplate";
+	
+	public PPAPWorkflowTemplatePageTest()
+	{
+		super();
+	}
+	
+	@BeforeMethod
+	public void setup() throws InterruptedException
+	{
+		initialize();
+		loginpage = new LoginPage();
+		homePage = loginpage.login(prop.getProperty("username"), prop.getProperty("password"));
+		ppapworkflowtemplatepage = homePage.clickOnPPAPTemplete();
+		
+	}
+
+	@Test(priority=1)
+	public void ppapTemplatePageTitleTest()
+	{
+		String pagetitle=ppapworkflowtemplatepage.validatePPAPWorkflowTemplatePageTitle();
+		Assert.assertEquals(pagetitle, "PPAP Workflow Templates - QLM");
+		System.out.println("PPAP template page title is:"+pagetitle);
+	}
+
+	@DataProvider
+	public Object[][] getPPAPTestData()
+	{
+		Object data[][]=TestUtil.getTestData(sheetName);
+		return data;
+	}
+
+	@Test(priority=2,dataProvider="getPPAPTestData")
+	public void validateCreatePPAPTemplate(String tempName,String subLevel) throws InterruptedException
+	{
+		Thread.sleep(3000);
+		ppapworkflowtemplatepage.clickOnCreateNewTemplateButton();
+		ppapworkflowtemplatepage.createNewTemplate(tempName, subLevel);
+	}
+	
+	
+	
+	 @AfterMethod
+	 public void tearDown()
+	 {
+	   driver.quit();
+	 }
+	
+
+}
